9bab09264792d64125680f0929cb2eac40080b99
revalian <revalian2@gmail.com>
2020-11-25 00:04:13 +0700

    commit

---
+++ app/src/main/java/dicoding/com/lian/mynavigationdrawer3/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package dicoding.com.lian.mynavigationdrawer3.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
